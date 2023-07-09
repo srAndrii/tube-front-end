4901e3f114598e7aed85c019b040e5d46cb75840
srAndrii <55akif5548@gmail.com>
2023-07-09 15:36:34 -0700

    Work with Auth Form

--- components/header/auth-form/AuthForm.tsx
+++ components/header/auth-form/AuthForm.tsx
@@ -1,6 +1,43 @@
-import { FC } from 'react'
+import stylesIcon from '../icons-right/IconsRight.module.scss'
+import styles from './AuthForm.module.scss'
+import { IAuthFields } from './auth-form.interface'
+import { useOutside } from '@/hooks/useOutside'
+import { FC, useState } from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { FaUserCircle } from 'react-icons/fa'
 
 export const AuthForm: FC = () => {
-	return <div>AuthForm</div>
+	const { isShow, ref, setIsShow } = useOutside(false)
+	const [type, setType] = useState<'login' | 'register'>('login')
+
+	const {
+		register,
+		formState: { errors },
+		handleSubmit
+	} = useForm<IAuthFields>({
+		mode: 'onChange'
+	})
+
+	const onSubmit: SubmitHandler<IAuthFields> = (data) => {
+		// if (type ==='login')
+		// 	else if (type==='register')
+	}
+
+	return (
+		<div className={styles.wrapper} ref={ref}>
+			<button
+				className={stylesIcon.button}
+				onClick={() => setIsShow(!isShow)}
+			>
+				<FaUserCircle fill='#A4A4A4' />
+			</button>
+			{isShow && (
+				<form
+					className={styles.form}
+					onSubmit={handleSubmit(onSubmit)}
+				></form>
+			)}
+		</div>
+	)
 }
 export default AuthForm
